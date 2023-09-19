2b97d315f93b433af4ac928327ffb0b7167dbd2d
JeremiasVillane <vil.jeremias@gmail.com>
2023-09-19 14:32:57 -0300

    added missing React imports

--- client/src/modules/user/feed/views/Feed.jsx
+++ client/src/modules/user/feed/views/Feed.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Animate, AnimateGroup } from "react-simple-animate";
 import Swal from "sweetalert2";
