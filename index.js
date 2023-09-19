60658f432a48563504fa002a0fbe308f927b59e8
JeremiasVillane <vil.jeremias@gmail.com>
2023-09-19 13:43:25 -0300

    server port env issue fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const appSocket = require("./src/app");
 const { conn } = require("./src/db");
 const { addAdmin } = require("./src/helpers/createUser");
 
-appSocket.listen(SV_PORT, async () => {
+appSocket.listen(PORT, async () => {
   await conn.sync({ alter: true });
   await addAdmin();
   console.log(`Server listen on ${PORT}`); // eslint-disable-line no-console
