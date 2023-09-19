6ba65f44dffcab2a6637ec5e044d2939db2a3730
JeremiasVillane <vil.jeremias@gmail.com>
2023-09-19 16:13:34 -0300

    cors *

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(morgan("dev"));
 //   })
 // );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", CL_URL);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
