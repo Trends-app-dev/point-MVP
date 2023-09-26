f88f21a4da96e611848ef0c9976e1e435a569a7a
unknown <ihourquescos@gmail.com>
2023-09-26 16:40:43 -0300

    arreglo bug de login

--- client/src/modules/auth/views/Login/Login.jsx
+++ client/src/modules/auth/views/Login/Login.jsx
@@ -10,137 +10,138 @@ import { validateLogin } from "../../utils";
 import styles from "./Login.module.css";
 
 const Login = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const darkMode = useSelector(selectDarkMode);
-  const MySwal = withReactContent(Swal);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const darkMode = useSelector(selectDarkMode);
+	const MySwal = withReactContent(Swal);
 
-  const [inputs, setInputs] = useState({
-    user: "",
-    password: "",
-  });
-  const [errors, setErrors] = useState({
-    user: "",
-  });
+	const [inputs, setInputs] = useState({
+		user: "",
+		password: "",
+	});
+	const [errors, setErrors] = useState({
+		user: "",
+	});
 
-  const handleChange = (event) => {
-    const { value, name } = event.target;
-    setInputs((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-    if (name === "user") {
-      setErrors((prevState) =>
-        validateLogin({
-          ...prevState,
-          [name]: value,
-        })
-      );
-    }
-  };
+	const handleChange = (event) => {
+		const { value, name } = event.target;
+		setInputs((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+		if (name === "user") {
+			setErrors((prevState) =>
+				validateLogin({
+					...prevState,
+					[name]: value,
+				})
+			);
+		}
+	};
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
 
-    if (inputs.user && inputs.password) {
-      try {
-        dispatch(loginUser(inputs)).then((result) => {
-          if (result.type === "company") navigate("/company/feed");
-          else if (result.type === "admin") navigate("/admin/dashboard");
-          else navigate("/user/feed");
-        });
-      } catch (error) {
-        MySwal.fire({
-          icon: "error",
-          position: "top-end",
-          toast: true,
-          title: error.response.data.error || "Error del servidor",
-          showConfirmButton: false,
-          timer: 2500,
-          timerProgressBar: true,
-          background: darkMode ? "#383636" : "#FFF",
-          color: darkMode ? "#FFF" : "#545454",
-          didOpen: (toast) => {
-            toast.addEventListener("mouseenter", Swal.stopTimer);
-            toast.addEventListener("mouseleave", Swal.resumeTimer);
-          },
-        });
-      }
-    }
-  };
+		if (inputs.user && inputs.password) {
+			try {
+				dispatch(loginUser(inputs)).then((result) => {
+					console.log("ENTRE A LOGIN", result);
+					if (result.type === "company") navigate("/company/feed");
+					else if (result.type === "admin") navigate("/admin/dashboard");
+					else navigate("/user/feed");
+				});
+			} catch (error) {
+				MySwal.fire({
+					icon: "error",
+					position: "top-end",
+					toast: true,
+					title: error.response.data.error || "Error del servidor",
+					showConfirmButton: false,
+					timer: 2500,
+					timerProgressBar: true,
+					background: darkMode ? "#383636" : "#FFF",
+					color: darkMode ? "#FFF" : "#545454",
+					didOpen: (toast) => {
+						toast.addEventListener("mouseenter", Swal.stopTimer);
+						toast.addEventListener("mouseleave", Swal.resumeTimer);
+					},
+				});
+			}
+		}
+	};
 
-  return (
-    <div className={styles.MajorContainer} style={{ userSelect: "none" }}>
-      <div className={styles.BGContainer}>
-        <div className={styles.Card}>
-          <div className={styles.LeftContainer}>
-            <div>
-              <h1>Bienvenido/a nuevamente,</h1>
-              <h3 className={styles.MainText}>
-                Una vez que inicies sesión encontrarás profesionales dispuestos
-                a compartir sus experiencias y estudiantes que están ingresando
-                al mundo laboral.
-              </h3>
-            </div>
-            <img src={welcome} alt="Bienvenido" />
-            <div className={styles.ExtraText}>
-              <h3>
-                Pero eso no es todo ¡También contamos con una sección para
-                empresas!
-              </h3>
-              <h3>
-                Próximamente experimentarás nuestra herramienta de búsqueda
-                automática de trabajo
-              </h3>
-            </div>
-          </div>
+	return (
+		<div className={styles.MajorContainer} style={{ userSelect: "none" }}>
+			<div className={styles.BGContainer}>
+				<div className={styles.Card}>
+					<div className={styles.LeftContainer}>
+						<div>
+							<h1>Bienvenido/a nuevamente,</h1>
+							<h3 className={styles.MainText}>
+								Una vez que inicies sesión encontrarás profesionales dispuestos
+								a compartir sus experiencias y estudiantes que están ingresando
+								al mundo laboral.
+							</h3>
+						</div>
+						<img src={welcome} alt="Bienvenido" />
+						<div className={styles.ExtraText}>
+							<h3>
+								Pero eso no es todo ¡También contamos con una sección para
+								empresas!
+							</h3>
+							<h3>
+								Próximamente experimentarás nuestra herramienta de búsqueda
+								automática de trabajo
+							</h3>
+						</div>
+					</div>
 
-          <div className={styles.RightContainer}>
-            <form onSubmit={handleSubmit}>
-              <h2>Ingresa con tu cuenta</h2>
-              <div className={styles.Input}>
-                <input
-                  name="user"
-                  onChange={handleChange}
-                  type="text"
-                  placeholder="Correo o nombre de usuario"
-                  autoComplete="username"
-                />
-                {errors.user && (
-                  <span className={styles.error}>{errors.user}</span>
-                )}
-              </div>
-              <div className={styles.Input}>
-                <input
-                  name="password"
-                  onChange={handleChange}
-                  type="password"
-                  placeholder="Contraseña"
-                  autoComplete="current-password"
-                />
-              </div>
-              <div className={styles.Options}>
-                {/* <div>
+					<div className={styles.RightContainer}>
+						<form onSubmit={handleSubmit}>
+							<h2>Ingresa con tu cuenta</h2>
+							<div className={styles.Input}>
+								<input
+									name="user"
+									onChange={handleChange}
+									type="text"
+									placeholder="Correo o nombre de usuario"
+									autoComplete="username"
+								/>
+								{errors.user && (
+									<span className={styles.error}>{errors.user}</span>
+								)}
+							</div>
+							<div className={styles.Input}>
+								<input
+									name="password"
+									onChange={handleChange}
+									type="password"
+									placeholder="Contraseña"
+									autoComplete="current-password"
+								/>
+							</div>
+							<div className={styles.Options}>
+								{/* <div>
                 <input id="remember" type="checkbox" />
                 <label htmlFor="remember"> Recuérdame</label>
               </div> */}
-                {/* <div>forgot Password</div> */}
-              </div>
-              <button
-                disabled={
-                  !(inputs.user && inputs.password) ||
-                  Object.keys(errors).length > 0
-                }
-                type="submit"
-              >
-                Ingresar
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+								{/* <div>forgot Password</div> */}
+							</div>
+							<button
+								disabled={
+									!(inputs.user && inputs.password) ||
+									Object.keys(errors).length > 0
+								}
+								type="submit"
+							>
+								Ingresar
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- client/src/redux/usersSlice.js
+++ client/src/redux/usersSlice.js
@@ -3,225 +3,224 @@ import axios from "axios";
 const { VITE_URL } = import.meta.env;
 
 const initialState = {
-  user: {},
-  allUsers: [],
-  searchedUsers: [],
-  students: {},
-  professionals: {},
-  companies: [],
+	user: {},
+	allUsers: [],
+	searchedUsers: [],
+	students: {},
+	professionals: {},
+	companies: [],
 };
 
 const usersSlice = createSlice({
-  name: "users",
-  initialState,
-  reducers: {
-    userLogout: () => {
-      return initialState;
-    },
-    matchUsers: (state) => {
-      const studentUsers = state.students.data || [];
-      const professionalUsers = state.professionals.data || [];
-      const combinedUsers = [...studentUsers, ...professionalUsers];
-      const sortedUsers = combinedUsers.sort(
-        (userA, userB) => userB.matchscore - userA.matchscore
-      );
-      const newUsers = sortedUsers.filter(
-        (newuser) =>
-          !state.allUsers.some(
-            (existingUser) => existingUser.user.id === newuser.user.id
-          )
-      );
-      state.allUsers = state.allUsers.concat(newUsers);
-    },
-    addCompany: (state, action) => {
-      state.companies = action.payload;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(updateUserProfile.pending, () => {})
-      .addCase(updateUserProfile.fulfilled, (state, action) => {
-        state.user = action.payload;
-      })
-      .addCase(getSearchedUsers.pending, (state) => {
-        state.searchedUsers = []; //Esto queda vacío porque despues podemos poner que si searchedUsers.length === 0 muestre un símbolo de carga
-      })
-      .addCase(getSearchedUsers.fulfilled, (state, action) => {
-        state.searchedUsers = action.payload;
-      })
-      .addCase(getUserInfo.pending, () => {})
-      .addCase(getUserInfo.fulfilled, (state, action) => {
-        state.user = action.payload;
-      })
-      .addCase(getStudents.fulfilled, (state, action) => {
-        state.students = action.payload;
-      })
-      .addCase(getStudents.pending, (state) => {})
-      .addCase(getStudents.rejected, (state, action) => {})
-      .addCase(getProfessionals.fulfilled, (state, action) => {
-        state.professionals = action.payload;
-      })
-      .addCase(getProfessionals.rejected, (state, action) => {});
-  },
+	name: "users",
+	initialState,
+	reducers: {
+		userLogout: () => {
+			return initialState;
+		},
+		matchUsers: (state) => {
+			const studentUsers = state.students.data || [];
+			const professionalUsers = state.professionals.data || [];
+			const combinedUsers = [...studentUsers, ...professionalUsers];
+			const sortedUsers = combinedUsers.sort(
+				(userA, userB) => userB.matchscore - userA.matchscore
+			);
+			const newUsers = sortedUsers.filter(
+				(newuser) =>
+					!state.allUsers.some(
+						(existingUser) => existingUser.user.id === newuser.user.id
+					)
+			);
+			state.allUsers = state.allUsers.concat(newUsers);
+		},
+		addCompany: (state, action) => {
+			state.companies = action.payload;
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(updateUserProfile.pending, () => {})
+			.addCase(updateUserProfile.fulfilled, (state, action) => {
+				state.user = action.payload;
+			})
+			.addCase(getSearchedUsers.pending, (state) => {
+				state.searchedUsers = []; //Esto queda vacío porque despues podemos poner que si searchedUsers.length === 0 muestre un símbolo de carga
+			})
+			.addCase(getSearchedUsers.fulfilled, (state, action) => {
+				state.searchedUsers = action.payload;
+			})
+			.addCase(getUserInfo.pending, () => {})
+			.addCase(getUserInfo.fulfilled, (state, action) => {
+				state.user = action.payload;
+			})
+			.addCase(getStudents.fulfilled, (state, action) => {
+				state.students = action.payload;
+			})
+			.addCase(getStudents.pending, (state) => {})
+			.addCase(getStudents.rejected, (state, action) => {})
+			.addCase(getProfessionals.fulfilled, (state, action) => {
+				state.professionals = action.payload;
+			})
+			.addCase(getProfessionals.rejected, (state, action) => {});
+	},
 });
 
 export const { userLogout, matchUsers, addCompany } = usersSlice.actions;
 
 export const getMatchedUsers = createAsyncThunk(
-  "users/getMatchedUsers",
-  async (page) => {
-    try {
-      const URL = `${VITE_URL}/search/users?type=student&page=${page}`;
-      const { data } = await axios.get(URL, { withCredentials: "include" });
+	"users/getMatchedUsers",
+	async (page) => {
+		try {
+			const URL = `${VITE_URL}/search/users?type=student&page=${page}`;
+			const { data } = await axios.get(URL, { withCredentials: "include" });
 
-      return data;
-    } catch (error) {
-      return error.response.data.error;
-    }
-  }
+			return data;
+		} catch (error) {
+			return error.response.data.error;
+		}
+	}
 );
 
 export const getStudents = createAsyncThunk(
-  "users/getStudents",
-  async ({ id, page }) => {
-    try {
-      const URL = `${VITE_URL}/user/feed/${id}/student?page=${page}`;
-      const { data } = await axios.get(URL, { withCredentials: "include" });
+	"users/getStudents",
+	async ({ id, page }) => {
+		try {
+			const URL = `${VITE_URL}/user/feed/${id}/student?page=${page}`;
+			const { data } = await axios.get(URL, { withCredentials: "include" });
 
-      return data;
-    } catch (error) {
-      return error.response.data.error;
-    }
-  }
+			return data;
+		} catch (error) {
+			return error.response.data.error;
+		}
+	}
 );
 
 export const getProfessionals = createAsyncThunk(
-  "users/getProfessionals",
-  async ({ id, page }) => {
-    try {
-      const URL = `${VITE_URL}/user/feed/${id}/professional?page=${page}`;
-      const { data } = await axios.get(URL, { withCredentials: "include" });
+	"users/getProfessionals",
+	async ({ id, page }) => {
+		try {
+			const URL = `${VITE_URL}/user/feed/${id}/professional?page=${page}`;
+			const { data } = await axios.get(URL, { withCredentials: "include" });
 
-      return data;
-    } catch (error) {
-      return error.response.data.error;
-    }
-  }
+			return data;
+		} catch (error) {
+			return error.response.data.error;
+		}
+	}
 );
 export const getUserInfo = createAsyncThunk("users/getUserInfo", async () => {
-  try {
-    const URL = `${VITE_URL}/user/profile`;
-    const { data } = await axios.get(URL, { withCredentials: "include" });
+	try {
+		const URL = `${VITE_URL}/user/profile`;
+		const { data } = await axios.get(URL, { withCredentials: "include" });
 
-    return data;
-  } catch (error) {
-    return error.response.data.error;
-  }
+		return data;
+	} catch (error) {
+		return error.response.data.error;
+	}
 });
 
 export const getSearchedUsers = createAsyncThunk(
-  "users/getSearchedUsers",
-  async ({ name, academic_formation, academic_institution }) => {
-    try {
-      let query = `${VITE_URL}/search/users?name=${name}`;
+	"users/getSearchedUsers",
+	async ({ name, academic_formation, academic_institution }) => {
+		try {
+			let query = `${VITE_URL}/search/users?name=${name}`;
 
-      if (academic_formation)
-        query += `&academic_formation=${academic_formation}`;
-      if (academic_institution)
-        query += `&academic_institution=${academic_institution}`;
-      const { data } = await axios.get(query);
+			if (academic_formation)
+				query += `&academic_formation=${academic_formation}`;
+			if (academic_institution)
+				query += `&academic_institution=${academic_institution}`;
+			const { data } = await axios.get(query);
 
-      return data;
-    } catch (error) {
-      return error.response.data.error;
-    }
-  }
+			return data;
+		} catch (error) {
+			return error.response.data.error;
+		}
+	}
 );
 
 export const updateUserProfile = createAsyncThunk(
-  "users/updateUserProfile",
-  async (editData) => {
-    try {
-      const URL = `${VITE_URL}/user/${editData.id}`;
-      const { data } = await axios.put(URL, editData, {
-        withCredentials: "include",
-      });
+	"users/updateUserProfile",
+	async (editData) => {
+		try {
+			const URL = `${VITE_URL}/user/${editData.id}`;
+			const { data } = await axios.put(URL, editData, {
+				withCredentials: "include",
+			});
 
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
-  }
+			return data;
+		} catch (error) {
+			console.error(error);
+		}
+	}
 );
 
 // Thunk para subir una imagen
 export const uploadImage = (formData) => async () => {
-  try {
-    const { data } = await axios.post(`${VITE_URL}/images/profile`, formData, {
-      withCredentials: "include",
-    });
-    const urlImage = data.imageUrl;
+	try {
+		const { data } = await axios.post(`${VITE_URL}/images/profile`, formData, {
+			withCredentials: "include",
+		});
+		const urlImage = data.imageUrl;
 
-    return urlImage;
-  } catch (error) {
-    console.error("Error subiendo imagen:", error);
-  }
+		return urlImage;
+	} catch (error) {
+		console.error("Error subiendo imagen:", error);
+	}
 };
 
 // Thunk para traer los datos de un usuario
 export const getSomeUserInfo = (userId) => async () => {
-  try {
-    const { data } = await axios.get(`${VITE_URL}/search/user/${userId}`, {
-      withCredentials: "include",
-    });
+	try {
+		const { data } = await axios.get(`${VITE_URL}/search/user/${userId}`, {
+			withCredentials: "include",
+		});
 
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 // Thunk para buscar usuarios por nombre y tipo
 export const searchUsers = (type, page, query) => async () => {
-  try {
-    const URL = `${VITE_URL}/search/users?type=${type}&page=${page}&name=${query}`;
-    const { data } = await axios.get(URL, { withCredentials: "include" });
+	try {
+		const URL = `${VITE_URL}/search/users?type=${type}&page=${page}&name=${query}`;
+		const { data } = await axios.get(URL, { withCredentials: "include" });
 
-    return data;
-  } catch (error) {
-    return error.response.data.error;
-  }
+		return data;
+	} catch (error) {
+		return error.response.data.error;
+	}
 };
 
 // Thunk para loguear un usuario
 export const loginUser = (inputs) => async (dispatch) => {
-  try {
-    const { data } = await axios.post(`${VITE_URL}/auth/login`, inputs, {
-      withCredentials: "include",
-    });
-    dispatch(getUserInfo()).then((result) => {
-      return result;
-    });
-  } catch (error) {
-    return error.response?.data?.error || error;
-  }
+	try {
+		const { data } = await axios.post(`${VITE_URL}/auth/login`, inputs, {
+			withCredentials: "include",
+		});
+		const result = await dispatch(getUserInfo());
+		return result;
+	} catch (error) {
+		return error.response?.data?.error || error;
+	}
 };
 
 // Thunk para registrar un usuario
 export const registerUser = (inputs) => async (dispatch) => {
-  try {
-    await axios.post(`${VITE_URL}/auth/register`, inputs);
-    dispatch(
-      loginUser({
-        user: inputs.email,
-        password: inputs.password,
-      })
-    ).then((result) => {
-      return result;
-    });
-  } catch (error) {
-    return error.response?.data?.error || error;
-  }
+	try {
+		await axios.post(`${VITE_URL}/auth/register`, inputs);
+		dispatch(
+			loginUser({
+				user: inputs.email,
+				password: inputs.password,
+			})
+		).then((result) => {
+			return result;
+		});
+	} catch (error) {
+		return error.response?.data?.error || error;
+	}
 };
 
 export const selectAllUsers = (state) => state.users?.allUsers;
